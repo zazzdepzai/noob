2e956513105a23324e03ad98b45e516b7fcf0420
zazzdepzai <pphucdeptrai4@gmail.com>
2026-09-11 13:16:01 +0700

    Update Minecraft.cpp

--- src/Minecraft.cpp
+++ src/Minecraft.cpp
@@ -1,10 +1,12 @@
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "Minecraft.h"
 #include "Settings.h"
-
 #include <windows.h>
 #include <winhttp.h>
 #include <shlobj.h>
-
 #include <string>
 #include <vector>
 #include <fstream>
